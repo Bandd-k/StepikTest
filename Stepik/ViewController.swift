6aea3c20d45810ce012553c1307a85bf016b367e
Bandd-k <banddk1@gmail.com>
2017-05-11 11:13:11 +0300

    Initial Commit

---
+++ Stepik/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stepik
+//
+//  Created by Denis Karpenko on 11.05.17.
+//  Copyright © 2017 Denis Karpenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
